db68a2fdbea0a5ba4149611a3bffad4702340daf
Edward Rees <erees.hk@gmail.com>
2023-09-01 22:16:06 -0700

    [feat] setup of route handlers for tickets

---
+++ src/app/tickets/[id]/route.ts
@@ -0,0 +1,27 @@
+import { createRouteHandlerClient } from "@supabase/auth-helpers-nextjs";
+import { cookies } from "next/headers";
+import type { Database } from "@/lib/database.types";
+
+import { NextRequest, NextResponse } from "next/server";
+
+const get_ticket = async (req: NextRequest, id: string) => {
+  const supabase = createRouteHandlerClient<Database>({ cookies });
+  const { data, error } = await supabase
+    .from("tickets")
+    .select("*")
+    .eq("id", id)
+    .single();
+
+  if (error) {
+    return NextResponse.error();
+  }
+
+  return NextResponse.json(data);
+};
+
+export async function GET(
+  req: NextRequest,
+  { params }: { params: { id: string } }
+) {
+  return get_ticket(req, params.id);
+}
---
+++ src/app/tickets/route.ts
@@ -0,0 +1,20 @@
+import { createRouteHandlerClient } from "@supabase/auth-helpers-nextjs";
+import { cookies } from 'next/headers';
+import type { Database } from "@/lib/database.types";
+
+import { NextRequest, NextResponse } from 'next/server';
+
+const get_tickets = async (req: NextRequest) => {
+  const supabase = createRouteHandlerClient<Database>({ cookies });
+  const { data, error } = await supabase
+    .from("tickets")
+    .select("*");
+  if(error){
+    return NextResponse.error();
+  }
+  return NextResponse.json(data);
+};
+
+export async function GET(req: NextRequest) {
+  return get_tickets(req);
+}
